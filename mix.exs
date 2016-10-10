ebc3477a0300aa66d42c33a21629748391b581a4
Dave Lively <davelively14@gmail.com>
2016-10-09 20:42:00 -0400

    Added Blitzy mod

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule Blitzy.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :httpoison, :timex]]
+    [mod: {Blitzy, []},
+     applications: [:logger, :httpoison, :timex]]
   end
 
   # Dependencies can be Hex packages:
