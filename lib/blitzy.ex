32c27541cfe2c9b3ae2e5be36f444ba736fe2df1
Dave Lively <davelively14@gmail.com>
2016-10-12 16:19:02 -0400

    Added Task for async running of Blitzy

--- lib/blitzy.ex
+++ lib/blitzy.ex
@@ -1,2 +1,14 @@
 defmodule Blitzy do
+
+  # Uses Task to asynchronosly spawn and execute a Blitzy Worker. Uses
+  # Task.await/2 to block the process until all results are gathered. Passes
+  # :infinity to await in order to avoid timeout issues. The HTTP client will
+  # timeout if the server takes too long, which is why we can use :infinity.
+  def run(n_workers, url) when n_workers > 0 do
+    worker_fun = fn -> Blitzy.Worker.start(url, self) end
+
+    1..n_workers
+    |> Enum.map(fn _ -> Task.async(worker_fun) end)
+    |> Enum.map(&Task.await(&1, :infinity))
+  end
 end
