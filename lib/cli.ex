adfabbb8123c9746f34dfbd2ef990ca81db02b82
Dave Lively <davelively14@gmail.com>
2016-10-14 13:18:46 -0400

    Starts nodes, added do_requests, called by process_options

--- lib/cli.ex
+++ lib/cli.ex
@@ -3,9 +3,19 @@ defmodule Blitzy.CLI do
   require Logger
 
   def main(args) do
+
+    # Application.get_env calls here will pull the :master_node and :slave_nodes
+    # from the config/config.exs file and starts the nodes.
+
+    Application.get_env(:blitzy, :master_node)
+    |> Node.start
+
+    Application.get_env(:blitzy, :slave_nodes)
+    |> Node.start
+
     args
     |> parse_args
-    |> process_options
+    |> process_options([node | Node.list])
   end
 
   # Thin wrapper for OptionParser.parse/2. Accepts a list of arguments and
@@ -24,17 +34,36 @@ defmodule Blitzy.CLI do
     OptionParser.parse(args, aliases: [n: :requests], strict: [requests: :integer])
   end
 
-  defp process_options(options) do
+  defp process_options(options, nodes) do
     case options do
 
       # Only executes if n is an integer, there are more args than just the num
       # of requests, and there are no invalid arguments.
       {[requests: n], [url], []} ->
-        IO.puts "Something"
-        # perform action
+        do_requests(n, url, nodes)
       _ ->
         do_help
     end
+  end
+
+  defp do_requests(n_requests, url, nodes) do
+    Logger.info "Pummeling #{url} with #{n_requests} requests"
+
+    total_nodes = Enum.count(nodes)
+    req_per_node = div(n_requests, total_nodes)
+
+    nodes
+    |> Enum.flat_map(fn node ->
+        1..req_per_node |> Enum.map(fn _ ->
+          Task.Supervisor.async({Blitzy.TaskSupervisor, node}, Blitzy.Worker, :start, [url])
+        end)
+      end)
+    |> Enum.map(&Task.await(&1, :infinity))
+    |> parse_results
+  end
+
+  defp parse_results(_) do
+    IO.puts "Parsing results"
   end
 
   # Displays to user what this app expects as input
